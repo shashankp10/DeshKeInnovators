7213dd0d464ed4d2778141910484ee0d8ced817a
Shashank Pandey <102212264+shashankp10@users.noreply.github.com>
2024-01-21 23:41:22 +0530

    Update MainBody2.js

--- src/Components/MainBody2.js
+++ src/Components/MainBody2.js
@@ -13,8 +13,31 @@ export default class MainBody2 extends PureComponent {
                 <div className="line"></div>
                 <p>Explore opportunities from across the globe to learn, <br/>showcase skills,gain CV points, & get hired by your dream company.</p>
             </div>
-            <div className="cards">
-                <CardGrid/>
+            <div className='card-grid'>
+              <div className='card'>
+                <a href="/mentorships"><h6>Mentorships</h6></a>
+                <img src={require('../assests/card1.webp')} alt="Teams" />
+              </div>
+              <div className='card'>
+                <a href="/learn"><h6>Learn</h6></a>
+                <img src={require('../assests/card2.webp')} alt="Teams" />
+              </div>
+              <div className='card'>
+                <a href="/teams"><h6>Teams</h6></a>
+                <img src={require('../assests/card3.webp')} alt="Teams" />
+              </div>
+              <div className='card'>
+                <a href="/ideas"><h6>Ideas</h6></a>
+                <img src={require('../assests/card4.webp')} alt="Teams" />
+              </div>
+              <div className='card'>
+                <a href="/built"><h6>Built</h6></a>
+                <img src={require('../assests/card5.webp')} alt="Teams" />
+              </div>
+              <div className='card'>
+                <a href="/blogs"> <h6>Blogs</h6></a>
+                <img src={require('../assests/card6.webp')} alt="Teams" />
+              </div>
             </div>
         </div>
       </>
